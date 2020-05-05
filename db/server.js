4494e0cea6d0c39fefd68d41b7906cd627ce72f7
DarkEye <lesko.matej.pu@gmail.com>
2020-05-06 01:22:22 +0200

    server autologin

--- db/server.js
+++ db/server.js
@@ -4,6 +4,7 @@ const jsonServer = require('json-server');
 const bcrypt = require('bcryptjs');
 const util = require('util');
 const cookieParser = require('cookie-parser');
+const pause = require('connect-pause');
 const redirects = require('./routes.json');
 const {
   assignJWTToken,
@@ -24,9 +25,27 @@ server.use(cookieSession);
 server.use(cookieParser());
 server.use(parseUserID);
 
+server.use(pause(650));
+
 server.use('/login', (req, res, next) => {
-  const { password, nickname } = req.body;
-  if (!password || !nickname) {
+  if (req.query.auto == 'true') {
+    const id = res.req.userID;
+    if (!id) {
+      return res.status(401).json({
+        data: null,
+        errors: [],
+      });
+    }
+    const user = router.db.get('users').getById(id).cloneDeep().value();
+    delete user.password;
+    return res.status(200).json({
+      data: user,
+      errors: null,
+    });
+  }
+
+  const { password, username } = req.body;
+  if (!password || !username) {
     return res.status(400).json({
       data: null,
       errors: [
@@ -35,7 +54,7 @@ server.use('/login', (req, res, next) => {
     });
   }
 
-  const user = router.db.get('users').find({ nickname }).value();
+  const user = router.db.get('users').find({ username }).cloneDeep().value();
 
   if (util.isUndefined(user)) {
     return res.status(401).json({
@@ -63,10 +82,10 @@ server.use('/login', (req, res, next) => {
     });
   }
 
-  const fUser = { ...user, password: undefined };
+  delete user.password;
   assignJWTToken(req.session, user.id);
   return res.status(200).json({
-    data: fUser,
+    data: user,
     errors: null,
   });
 });
