74e60e894a89f2b489d0d9c0085b968ee7d21951
DarkEye <lesko.matej.pu@gmail.com>
2020-05-06 01:22:35 +0200

    login prototype working

--- src/features/users/Login.tsx
+++ src/features/users/Login.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { connect } from 'react-redux';
 import { RootState } from '..';
 import { User } from '../../types/users';
@@ -7,33 +7,54 @@ import { loginUser } from './userSlice';
 
 const mapStateToProps = (state: RootState) => ({
   loggedUser: state.users.loggedUser,
+  loading: state.users.loading,
 });
 
 const mapDispatchToProps = (dispatch: AppDispatch) => {
   return {
-    handleLogIn: () =>
-      dispatch(loginUser({ nickname: 'Matej', password: 'abeceda' })),
+    logIn: (username: string, password: string) =>
+      dispatch(loginUser({ username, password })),
   };
 };
 
 type LoginProps = ReturnType<typeof mapStateToProps> &
   ReturnType<typeof mapDispatchToProps>;
 
-const Login: FC<LoginProps> = ({ loggedUser, handleLogIn }) => {
+const Login: FC<LoginProps> = ({ loggedUser, loading, logIn }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  if (loading) {
+    return <div>loading</div>;
+  }
+
   if (loggedUser) {
     loggedUser = loggedUser as User;
     console.log(loggedUser);
     return (
       <>
-        <div>I'm Here {loggedUser.nickname}</div>
+        <div>I'm Here {loggedUser.username}</div>
       </>
     );
   }
+
   return (
-    <>
-      <div>not logged in</div>
-      <button onClick={handleLogIn}>log in</button>
-    </>
+    <div>
+      <form>
+        <input
+          placeholder="nickname"
+          onInput={(e) => setUsername(e.currentTarget.value)}
+        ></input>
+        <input
+          placeholder="password"
+          type="password"
+          onInput={(e) => setPassword(e.currentTarget.value)}
+        ></input>
+      </form>
+      <button onClick={() => logIn(username, password)} type="submit">
+        log in
+      </button>
+    </div>
   );
 };
 
