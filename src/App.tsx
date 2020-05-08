e073f0b2fc5b886a8eec71e1aba6287bcebf7343
DarkEye <lesko.matej.pu@gmail.com>
2020-05-08 18:18:52 +0200

    login - styling and validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { NoSsr, Container } from '@material-ui/core';
 import AutoLogin from './components/AutoLogin';
 import Routes from './routes';
 
 function App() {
   return (
-    <>
+    <NoSsr>
       <AutoLogin></AutoLogin>
-      <Router>
-        <Routes></Routes>
-      </Router>
-    </>
+      <Container>
+        <Router>
+          <Routes></Routes>
+        </Router>
+      </Container>
+    </NoSsr>
   );
 }
 
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,7 +1,38 @@
 import React, { FC, useState } from 'react';
 import { RouteProps, Redirect } from 'react-router-dom';
+import {
+  Button,
+  Typography,
+  Grid,
+  Backdrop,
+  CircularProgress,
+  InputAdornment,
+  makeStyles,
+  createStyles,
+  IconButton,
+} from '@material-ui/core';
+import { Visibility, VisibilityOff, AccountCircle } from '@material-ui/icons';
+import { Formik, Form, Field } from 'formik';
+import { TextField } from 'formik-material-ui';
 import LoginFeature, { LoginProps } from '../features/users/Login';
 import { PAGES } from '../routes';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    padded: {
+      [theme.breakpoints.up('md')]: {
+        paddingBottom: '5rem',
+      },
+      paddingTop: '5rem',
+      paddingBottom: '1rem',
+    },
+  }),
+);
+
+interface FormValidationValues {
+  username: string;
+  password: string;
+}
 
 const Login: FC<LoginProps & RouteProps> = ({
   loggedUser,
@@ -9,35 +40,131 @@ const Login: FC<LoginProps & RouteProps> = ({
   logIn,
   location,
 }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const classes = useStyles();
   const { from }: any = location?.state || { from: { pathname: PAGES.home } };
-
-  if (loading) {
-    return <div>loading</div>;
-  }
 
   if (loggedUser) {
     return <Redirect to={from} />;
   }
 
   return (
-    <div>
-      <form>
-        <input
-          placeholder="nickname"
-          onInput={(e) => setUsername(e.currentTarget.value)}
-        ></input>
-        <input
-          placeholder="password"
-          type="password"
-          onInput={(e) => setPassword(e.currentTarget.value)}
-        ></input>
-      </form>
-      <button onClick={() => logIn(username, password)} type="submit">
-        log in
-      </button>
-    </div>
+    <>
+      <Grid container alignItems="center" direction="column" spacing={1}>
+        <Grid item>
+          <Typography variant="h1" className={classes.padded}>
+            Log In
+          </Typography>
+        </Grid>
+        <Formik
+          initialValues={{
+            username: '',
+            password: '',
+          }}
+          onSubmit={({ username, password }, { setSubmitting }) => {
+            logIn(username, password);
+            setSubmitting(false);
+          }}
+          validate={({ username, password }) => {
+            const errors: Partial<FormValidationValues> = {};
+            if (!username) {
+              errors.username = 'Required';
+            }
+            if (!password) {
+              errors.password = 'Required';
+            }
+            console.log(errors);
+            return errors;
+          }}
+        >
+          {({ submitForm, isValid, dirty, errors, touched }) => (
+            <Grid
+              container
+              item
+              component={Form}
+              direction="column"
+              xs={12}
+              sm={6}
+              spacing={1}
+            >
+              <Grid item>
+                <Field
+                  component={TextField}
+                  id="username"
+                  name="username"
+                  variant="outlined"
+                  color="primary"
+                  autoFocus
+                  fullWidth
+                  label="Username"
+                  disabled={loading}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton disabled>
+                          <AccountCircle
+                            color={
+                              errors.username && touched.username
+                                ? 'secondary'
+                                : 'primary'
+                            }
+                          />
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                ></Field>
+              </Grid>
+              <Grid item>
+                <Field
+                  component={TextField}
+                  id="password"
+                  name="password"
+                  variant="outlined"
+                  type={passwordVisible ? 'text' : 'password'}
+                  fullWidth
+                  label="Password"
+                  disabled={loading}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment
+                        position="end"
+                        onClick={() => setPasswordVisible(!passwordVisible)}
+                      >
+                        <IconButton
+                          color={
+                            errors.password && touched.password
+                              ? 'secondary'
+                              : 'primary'
+                          }
+                        >
+                          {passwordVisible ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                ></Field>
+              </Grid>
+              <Grid item justify="flex-end" container>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={submitForm}
+                  type="submit"
+                  disabled={loading || !dirty || !isValid}
+                  startIcon={loading && dirty && <CircularProgress size={20} />}
+                >
+                  Log{loading && dirty && 'ing'} In
+                </Button>
+              </Grid>
+            </Grid>
+          )}
+        </Formik>
+      </Grid>
+      <Backdrop open={loading}>
+        <CircularProgress />
+      </Backdrop>
+    </>
   );
 };
 
