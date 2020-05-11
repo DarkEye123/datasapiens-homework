b32bebe737d2af1bab19eb0d675afaf7b72fb5f1
DarkEye <lesko.matej.pu@gmail.com>
2020-05-12 00:48:37 +0200

    responsivity tweaks

--- src/components/Buttons/Add.tsx
+++ src/components/Buttons/Add.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { styled, Theme, Fab } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
-const AddButton = styled((props) => (
-  <Fab color="primary" aria-label="add entry" {...props}>
-    <AddIcon />
-  </Fab>
-))(({ theme }: { theme: Theme }) => ({
+const AddButton = styled((props) => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
+  return (
+    <Fab
+      color="primary"
+      aria-label="add entry"
+      size={matches ? 'large' : 'small'}
+      {...props}
+    >
+      <AddIcon />
+    </Fab>
+  );
+})(({ theme }: { theme: Theme }) => ({
   position: 'fixed',
   bottom: theme.spacing(2),
   right: theme.spacing(2),
--- src/components/Graphs/Donut/tooltip.tsx
+++ src/components/Graphs/Donut/tooltip.tsx
@@ -1,16 +1,37 @@
 import React, { FC } from 'react';
-import { Typography, Grid, makeStyles, createStyles } from '@material-ui/core';
+import {
+  Typography,
+  Grid,
+  makeStyles,
+  createStyles,
+  Divider,
+} from '@material-ui/core';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import { DonutGraphCategory } from '../../../types/budget';
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme) =>
   createStyles({
     root: {
-      width: '300px',
+      width: '150px',
+      [theme.breakpoints.up('sm')]: {
+        width: '300px',
+      },
     },
     square: {
       color: (props: { color: string }) => props.color,
       fontSize: '1.2rem',
+    },
+    icon: {
+      fontSize: '16px',
+      [theme.breakpoints.up('sm')]: {
+        fontSize: '32px',
+      },
+    },
+    typography: {
+      fontSize: '10px',
+      [theme.breakpoints.up('sm')]: {
+        fontSize: theme.typography.fontSize,
+      },
     },
   }),
 );
@@ -24,14 +45,24 @@ const Tooltip: FC<DonutGraphCategory & { color: string }> = ({
   return (
     <Grid container className={classes.root}>
       <Grid item xs={2} container justify="center" alignContent="center">
-        <InfoOutlinedIcon fontSize="large"></InfoOutlinedIcon>
+        <InfoOutlinedIcon className={classes.icon}></InfoOutlinedIcon>
       </Grid>
       <Grid item xs={10}>
-        <Typography variant="caption" display="block" gutterBottom>
+        <Typography
+          variant="caption"
+          display="block"
+          className={classes.typography}
+          gutterBottom
+        >
           to select category double click on graph area
         </Typography>
-        <hr></hr>
-        <Typography variant="caption" display="block" gutterBottom>
+        <Divider></Divider>
+        <Typography
+          variant="caption"
+          display="block"
+          className={classes.typography}
+          gutterBottom
+        >
           <span className={classes.square}>■</span>
           {id}:<b>{value}</b>
         </Typography>
